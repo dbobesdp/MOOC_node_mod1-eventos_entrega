9b562ed6753c460373607b641a2fae115791ee94
Daniel Bobes de Pedro <dbobesdp@gmail.com>
2021-02-12 14:08:41 +0100

    Añadido ProgramadorJS

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Habitacion = require('./habitacion');
 const Climatizador = require('./climatizador');
 const Termostato = require('./termostato');
+const Programador = require('./programador');
 
 // Creamos una habitacion:
 const dormitorio = new Habitacion();
@@ -12,6 +13,19 @@ const climatizador = new Climatizador(dormitorio);
 // Creamos un Termostato que mira la temperatura de la habitacion:
 const termostato = new Termostato(dormitorio);
 
+// Configuración de horas y temperaturas que se desea programar
+const configuracion = [
+  { hora: '02:32', temperatura: 22 },
+  { hora: '02:33', temperatura: 18 },
+  //   { hora: '07:00', temperatura: 22 },
+  //   { hora: '08:30', temperatura: 18 },
+  //   { hora: '18:00', temperatura: 22 },
+  //   { hora: '23:00', temperatura: 20 },
+];
+
+// Creamos un programador que configurará la temperatura deseada en todo momento
+const programador = new Programador(configuracion);
+
 // Configuramos el termostato para controlar la temperatura:
 termostato.on('muchofrio', () => climatizador.calentar());
 termostato.on('muchocalor', () => climatizador.enfriar());
@@ -22,5 +36,14 @@ termostato.on('tic', (temp) => console.log(`${temp.toFixed(1)}ºC`));
 // Configurar la temp ideal a 20 grados:
 termostato.indicarTemperaturaIdeal(20);
 
+// Configurar el programador para programar la temperatura ideal
+programador.on('ideal', (temp) => {
+  termostato.indicarTemperaturaIdeal(temp);
+  console.log(`Cambio programado de temperatura ideal a ${temp.toFixed(1)}ºC`);
+});
+
 // Encender el termostato:
 termostato.encender();
+
+// Encender el programador
+programador.encender();
---
+++ programador.js
@@ -0,0 +1,33 @@
+const EventEmitter = require('events');
+const later = require('later');
+
+class Programador extends EventEmitter {
+  constructor(configuracion = []) {
+    super();
+
+    // Configuración de horas y temperaturas
+    this.configuracion = configuracion;
+
+    // Zona horaria
+    this.indicarZonaHoraria();
+  }
+
+  indicarZonaHoraria() {
+    // Usar zona horaria local:
+    later.date.localTime();
+  }
+
+  encender() {
+    console.log('Encendiendo el programador.');
+
+    this.configuracion.forEach((conf) => {
+      const { hora, temperatura } = conf;
+      const sched = later.parse.text(`at ${hora}`);
+      later.setInterval(() => {
+        this.emit('ideal', temperatura);
+      }, sched);
+    });
+  }
+}
+
+exports = module.exports = Programador;
